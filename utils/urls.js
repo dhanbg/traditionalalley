8bcb1e4c5d2fe0b81eb3a2fd69e3b37a7ef6e7fd
dhanbg <rohitgurung0915@gmail.com>
2025-04-02 16:55:03 +0545

    Update urls.js

--- utils/urls.js
+++ utils/urls.js
@@ -1,3 +1,3 @@
 export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://admin.traditionalalley.com.np/admin/";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://admin.traditionalalley.com.np";
