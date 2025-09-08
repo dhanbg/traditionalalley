8702fd98350aef262f69fb179d8b5a5151e1a445
rohitgurung1 <rohitgurung1>
2025-09-08 12:21:50 +0545

    Fixed Metadata for google

--- app/client-layout.jsx
+++ app/client-layout.jsx
@@ -163,9 +163,6 @@ export default function RootLayout({ children }) {
   return (
     <SessionProvider>
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </head>
       <body suppressHydrationWarning={true}>
         <ToastProvider>
           <Context>
--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,13 @@ import ClientLayout from './client-layout';
 
 export const metadata = defaultMetadata;
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+};
+
 export default function RootLayout({ children }) {
   return <ClientLayout>{children}</ClientLayout>;
 }
